03132db5f3742feb70e73e359128b95dd31662aa
tracek <lukasz.tracewski@gmail.com>
2016-07-10 09:03:51 +0200

    Somewhat working

--- batch_uploader.py
+++ batch_uploader.py
@@ -14,15 +14,33 @@ import requests
 
 
 def load_metadata_from_csv(path):
+    """
+    Grabs properties from the give csv file. The csv should be organised as follows:
+    filename (without extension), property1, property2, ...
+
+    Example:
+    id_no,class,category,binomial
+    my_file_1,GASTROPODA,EN,Aaadonta constricta
+    my_file_2,GASTROPODA,CR,Aaadonta irregularis
+
+    The corresponding files are my_file_1.tif and my_file_2.tif.
+
+    The program will turn the above into a json object:
+
+    { id_no: my_file_1, class: GASTROPODA, category: EN, binomial: Aaadonta constricta},
+    { id_no: my_file_2, class: GASTROPODA, category: CR, binomial: Aaadonta irregularis}
+
+    :param path to csv:
+    :return: dictionary of dictionaries
+    """
     with open(path, mode='r') as metadata_file:
         reader = csv.reader(metadata_file)
         header = reader.next()
-        result = {row[0]: dict(zip(header, row)) for row in reader}
-        return result
+        metadata = {row[0]: dict(zip(header, row)) for row in reader}
+        return metadata
 
 
 def setup_logging(path, default_level=logging.INFO):
-
     if os.path.exists(path):
         with open(path, 'rt') as f:
             config = json.load(f)
@@ -30,8 +48,8 @@ def setup_logging(path, default_level=logging.INFO):
     else:
         logging.basicConfig(level=default_level)
 
-def get_google_auth_session(username, password):
 
+def get_google_auth_session(username, password):
     google_accounts_url = 'https://accounts.google.com'
     authentication_url = 'https://accounts.google.com/ServiceLoginAuth'
 
@@ -58,23 +76,23 @@ def get_google_auth_session(username, password):
         logging.critical("Logging failed")
         sys.exit(1)
 
+
 def get_upload_url(session):
-    appspot_url = 'https://ee-api.appspot.com/assets/upload/geturl?'
-    r = session.get(appspot_url)
+    r = session.get('https://ee-api.appspot.com/assets/upload/geturl?')
     return r.json()['url']
+
 
 def upload_file(session, file_path, asset_name, properties=None):
     files = {'file': open(file_path, 'rb')}
-    uploadurl = get_upload_url(session)
-
-    r = session.post(uploadurl, files=files)
-
-    gsid = r.json()[0]
+    upload_url = get_upload_url(session)
+    upload = session.post(upload_url, files=files)
+    gsid = upload.json()[0]
     asset_data = {"id": asset_name,
                   "tilesets": [
                       {"sources": [
                           {"primaryPath": gsid,
-                           "additionalPaths": []}
+                           "additionalPaths": []
+                          }
                       ]}
                   ],
                   "bands": [],
@@ -82,11 +100,12 @@ def upload_file(session, file_path, asset_name, properties=None):
                   "reductionPolicy": "MEAN"}
     return asset_data
 
+
 def collection_exist(path):
     return True if ee.data.getInfo(path) else False
 
-def create_image_collection(root, name):
-    full_path_to_collection = os.path.join(root, name)
+
+def create_image_collection(full_path_to_collection):
     if collection_exist(full_path_to_collection):
         logging.warning("Collection %s already exists", full_path_to_collection)
     else:
@@ -94,7 +113,11 @@ def create_image_collection(root, name):
         logging.info('New collection %s created', full_path_to_collection)
 
 
-if __name__ == '__main__':
+def get_filename_from_path(path):
+    return os.path.splitext(os.path.basename(os.path.normpath(path)))[0]
+
+
+def main(argv):
     setup_logging(path='logconfig.json')
     parser = argparse.ArgumentParser(description='Google Earth Engine Asset Manager', prog='GEE asset manager')
     parser.add_argument('-u', '--user', help='Google account name (gmail address)', required=True)
@@ -103,20 +126,33 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--collection', help='Name of the collection to create')
     args = parser.parse_args()
 
-    image_collection_name = args.collection or os.path.basename(os.path.normpath(args.directory))
+    image_collection_name = args.collection or get_filename_from_path(args.directory)
 
     ee.Initialize()
+
     root_path_in_gee = ee.data.getAssetRoots()[0]['id']
-    create_image_collection(root_path_in_gee, image_collection_name)
+
+    full_path_to_collection = os.path.join(root_path_in_gee, image_collection_name)
+
+    create_image_collection(full_path_to_collection)
 
     password = getpass()
 
-    session = get_google_auth_session(args.user, password)
+    google_session = get_google_auth_session(args.user, password)
 
-    for images in glob.glob(os.path.join(args.directory, '*.tif')):
-        pass
+    metadata = load_metadata_from_csv(args.properties)
 
-    asset_request = upload_file(session, img, 'users/rspb/test_collection/test_lucy8')
+    for image in glob.glob(os.path.join(args.directory, '*.tif')):
+        logging.info('Processing %s', image)
+        filename = get_filename_from_path(path=image)
+        properties = metadata[filename]
+        asset_request = upload_file(session=google_session,
+                                    file_path=image,
+                                    asset_name=os.path.join(full_path_to_collection, filename),
+                                    properties=properties)
+        task_id = ee.data.newTaskId(1)[0]
+        r = ee.data.startIngestion(task_id, asset_request)
 
-    taskid = ee.data.newTaskId(1)[0]
-    ret = ee.data.startIngestion(taskid, asset_request)
+
+if __name__ == '__main__':
+    main(sys.argv)
