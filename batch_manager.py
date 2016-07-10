ba6b29cb5466618ff83873273f376b66ffdec13a
tracek <lukasz.tracewski@gmail.com>
2016-07-10 23:24:53 +0200

    Adding subcommands

--- batch_manager.py
+++ batch_manager.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 import ee
@@ -24,5 +25,25 @@ def cancel_all_running_tasks():
     for task in running_tasks:
         ee.data.cancelTask(task['id'])
     logging.info('Cancel all request completed')
+
+
+def delete(args):
+    print(args)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Google Earth Engine Batch Asset Manager', prog='GEE asset manager')
+    # parser.add_argument('function', choices=['cancell', 'delete'])
+    # delete_parser = parser.add_subparsers(help='Delete collection')
+    # delete_parser.add_parser('delete', help='ID of the collection')
+    # delete_parser.set_defaults(func=delete)
+    # cancel_parser = parser.add_subparsers(help='Cancell all')
+    # cancel_parser.add_parser('cancel', action='store_true')
+
+    subparsers = parser.add_subparsers()
+    parser_delete = subparsers.add_parser('delete')
+    parser_delete.add_argument('id')
+    parser_delete.set_defaults(func=delete)
+    parser.parse_args()
 
 
