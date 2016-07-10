80ef08d22945967afb3f023152b493a3ab9b72d1
tracek <lukasz.tracewski@gmail.com>
2016-07-10 16:26:35 +0200

    Add a method to cancel all tasks

--- utilities.py
+++ utilities.py
@@ -1,4 +1,8 @@
+import logging
+
+
 def delete_collection(ee, id):
+    logging.info('Attempting to delete collection %s', id)
     if 'users' not in id:
         root_path_in_gee = ee.data.getAssetRoots()[0]['id']
         id = root_path_in_gee + '/' + id
@@ -7,3 +11,12 @@ def delete_collection(ee, id):
     for item in items_in_collection:
         ee.data.deleteAsset(item['id'])
     ee.data.deleteAsset(id)
+    logging.info('Collection %s removed', id)
+
+
+def cancel_all_running_tasks(ee):
+    logging.info('Attempting to cancel all running tasks')
+    running_tasks = [task for task in ee.data.getTaskList() if task['state'] == 'RUNNING']
+    for task in running_tasks:
+        ee.data.cancelTask(task['id'])
+    logging.info('Cancel all request completed')
