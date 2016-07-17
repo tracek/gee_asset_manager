147a8a91ebd0f5c0a2a5c416c9f9d70494c41642
tracek <lukasz.tracewski@gmail.com>
2016-07-17 20:01:36 +0200

    Add new requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='gee_asset_manager',
     version='0.0.1',
-    packages=[''],
+    packages=['batch_manager'],
     url='https://github.com/tracek/gee_asset_manager',
     license='Apache 2.0',
     author='Lukasz Tracewski',
     author_email='lukasz.tracewski@outlook.com',
     description='Google Earth Engine assets manager',
     install_requires=[
-        'earthengine_api',
+        'retrying',
         'requests',
         'ee'
     ]
