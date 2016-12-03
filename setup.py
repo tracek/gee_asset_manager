cdea01bfbf52641f9a8a1e0577f4b2e5996fdd30
tracek <lukasz.tracewski@gmail.com>
2016-12-03 08:53:00 +0100

    Remove distutils support

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 
 setup(
     name='geebam',
-    version='0.1.1',
+    version='0.1.2',
     packages=['gee_asset_manager'],
-    # package_dir=['gee_asset_manager'],
     package_data={'gee_asset_manager': ['logconfig.json']},
     url='https://github.com/tracek/gee_asset_manager',
     license='Apache 2.0',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+    ),
     author='Lukasz Tracewski',
     author_email='lukasz.tracewski@outlook.com',
     description='Google Earth Engine Batch Assets Manager',
+    long_description=readme(),
     install_requires=[
         'retrying',
         'requests',
@@ -25,6 +39,5 @@ setup(
         'console_scripts': [
             'geebam=geebam:main',
         ],
-    },
-    scripts=['geebam.py']
+    }
 )
